f59cfac52d23d1515c288373e4e809c543297e00
Manoel Ribeiro <ribeiromanoel2003@gmail.com>
2023-12-26 19:36:08 -0300

    adicionado posição da devolucao

--- app4_tela.py
+++ app4_tela.py
@@ -21,27 +21,35 @@ def carregar_navegador():
 
 def fazer_devolucao():
     nav.fazer_devolucao(prod.array_cod_prod,prod.array_lote, prod.array_quant_result)
+    atualizar_contador()
    
 def print_dados_planilha():
     print(nav.dados_planilha(prod))
     
 def inserir_codigo():
     nav.inserir_codigo(prod.array_cod_prod)
+    atualizar_contador()
 
 def sair():
     window.destroy()
     
 def pular_linha():
     nav.contador()
-    label_info.config(text="Linha: "+str(nav.get_contador()+2))
+    atualizar_contador()
+    
+def voltar_linha():
+    nav.descontador()
+    atualizar_contador()
 
 def alterar_linha():
-    nova_linha = int(campo_texto.get())
-    if nova_linha == None or nova_linha == 0:
+    nova_linha = campo_texto.get()
+    if nova_linha == None or nova_linha == "" or nova_linha == 0:
         nova_linha = 1
-    nav.alter_contador(nova_linha-2)
-    label_info.config(text="Linha: "+str(int(nav.get_contador()+2)))
+    nav.alter_contador(int(nova_linha)-2)
+    atualizar_contador()
 
+def atualizar_contador():
+    label_info.config(text="Linha: "+str(int(nav.get_contador()+2)))
 
 window = Tk()
 
@@ -60,11 +68,14 @@ notebook.add(tab1,text="Tab 1")
 notebook.add(tab2,text="Tab 2")
 
 # Criando um botão
+botao = Button(tab1, text="Voltar linha", command=voltar_linha)
+botao.pack(side=TOP, padx=5)
+# Criando um botão
 botao = Button(tab1, text="Pular linha", command=pular_linha)
 botao.pack(side=TOP, padx=5)
 
 # Criando uma label
-label_info = Label(tab1, text="Linha: "+str(nav.get_contador()))
+label_info = Label(tab1,font=('Arial', 10) ,text="Linha: "+str(int(nav.get_contador()+2)))
 label_info.pack(side=TOP, padx=5)
 
 # Criando um campo de texto
@@ -77,12 +88,12 @@ botao_alterar.pack(side=TOP, padx=5)
 
 Label(tab2, text='Inserir dados', width= 22, height=10, bg='light yellow').pack()
 
-Button(tab1, text='Abrir Planilha', font=('Arial', 15), command=abrir_planilha).pack(padx=8, pady=8, fill=BOTH, )
-Button(tab1, text='Carregar Navegador', font=('Arial', 15), command=carregar_navegador).pack(padx=8, pady=8, fill=BOTH)
-Button(tab1, text='Inserir Código', font=('Arial', 15), command=inserir_codigo).pack(padx=8, pady=8, fill=BOTH)
-Button(tab1, text='Fazer Devolução', font=('Arial', 15), command=fazer_devolucao).pack(padx=8, pady=8, fill=BOTH)
+Button(tab1, text='Abrir Planilha',foreground="#FFFFFF",background='#006633', font=('Arial', 15), command=abrir_planilha).pack(padx=8, pady=8, fill=BOTH, )
+Button(tab1, text='Carregar Navegador',foreground="#FFFFFF",background='#A57A0F' ,font=('Arial', 15), command=carregar_navegador).pack(padx=8, pady=8, fill=BOTH)
+Button(tab1, text='Inserir Código', foreground="#FFFFFF",background='#0D214F', font=('Arial', 15), command=inserir_codigo).pack(padx=8, pady=8, fill=BOTH)
+Button(tab1, text='Fazer Devolução', foreground="#FFFFFF",background='#0D214F',font=('Arial', 15), command=fazer_devolucao).pack(padx=8, pady=8, fill=BOTH)
 Button(tab1, text='Mostrar quantidades', font=('Arial', 15), command=mostrarQuantidades).pack(padx=8, pady=8, fill=BOTH, )
 Button(tab1, text='Mostrar dados planilha', font=('Arial', 15), command=print_dados_planilha).pack(padx=8, pady=8, fill=BOTH, )
-Button(tab1, text='Sair', font=('Arial', 15), command=sair).pack(padx=8, pady=8, fill=BOTH, )
+Button(tab1, text='Sair',foreground="#FFFFFF",background='#8B0000', font=('Arial', 15, ), command=sair).pack(padx=8, pady=8, fill=BOTH, )
 
 window.mainloop()
