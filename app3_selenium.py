8a7d1f7fcac9418880b2de5edeb54f1a03e00276
Manoel Ribeiro <ribeiromanoel2003@gmail.com>
2023-12-26 01:34:09 -0300

    commit dia 25/12

--- app3_selenium.py
+++ app3_selenium.py
@@ -17,6 +17,17 @@ class Navegador():
     def contador(self):
         self.linha = self.linha + 1
         
+    def descontador(self):
+        self.linha = self.linha - 1
+        
+    def alter_contador(self, num: int):
+        self.linha = num
+        if self.linha < 0:
+            self.linha = 0
+    
+    def get_contador(self):
+        return self.linha
+        
     def dados_planilha(self, obj: Produtos):
         dados = [obj.array_cod_prod, obj.array_lote, obj.array_nome_prod, obj.array_quant_result, obj.tamanho_devolucao]
         return dados
@@ -26,104 +37,83 @@ class Navegador():
         pagina = 'https://amberlive.000webhostapp.com/pagina.html'
         self.driver.get(pagina)
     
-    def fazer_devolucao(self, array_cod_prod: list, array_lote: list, array_quant_result: list):
-            
-        # Localizo o RadioButton de Devolucao ✓
-        bot_dev = self.driver.find_element(By.ID, "OriginalAddress_Devolution")
-        # Clico nesse botão ✓
-        bot_dev.click()
-        # E espero um pouco ✓
-        time.sleep(0.2)
-        #---------------------------------------------------------------------------------------------------------
+    def inserir_codigo(self, array_cod_prod):
+        try:
+            # Localizo o campo input de produtos ✓
+            prod = self.driver.find_element(By.ID, "Product_Name")
+            # Clico nesse input ✓
+            prod.click()
+            # Limpo esse input, para evitar qualquer coisa ✓
+            prod.clear()
+            # Digito o codigo do produto ✓
+            prod.send_keys(array_cod_prod[self.linha])
+        except:
+            print('Não foi possível inserir o código do produto')
         
-        # Localizo o campo input de produtos ✓
-        prod = self.driver.find_element(By.ID, "Product_Name")
-        # Clico nesse input ✓
-        prod.click()
-        # Limpo esse input, para evitar qualquer coisa ✓
-        prod.clear()
-        # E espero um pouco ✓
-        time.sleep(0.2)
-        # Digito o codigo do produto ✓
-        prod.send_keys(array_cod_prod[self.linha])
-        #
-        # Caso não queira usar o WebDriver Wait: time.sleep(3)
-        #
-        # ANALISAR SE NÃO SERIA MELHOR ESCREVER O CÓDIGO E TAMBÉM O NOME: prod.send_keys(array_cod_prod[self.linha], " - ", array_nome_prod[linha])
-        #
-        # WebDriver Wait para esperar aparecer o produto na pesquisa
-        WebDriverWait(self.driver, 15).until(
-            EC.presence_of_element_located((By.XPATH, "xpath do produto que aparecer"))
-        )
-        #
-        # Talvez assim seja mais facil
-        # if ('elemento nao apareceu'):
-        #   break    
-        #
-        #Elemento apareceu? Então aperta Enter
-        prod.send_keys(Keys.RETURN)        
-        #---------------------------------------------------------------------------------------------------------
+    
+    def fazer_devolucao(self, array_cod_prod: list,array_lote: list, array_quant_result: list):
         
-        
-        #
-        # -> Olhar melhor o xpath desses botões (saber se é realmente esse, porque exitiam 2 botões de cada no site offline)
-        #
-        # Xpath dos botões Dropdowns
-        xpath_dropdowns = '//button[@class="pui-button ui-widget ui-state-default ui-corner-right pui-button-icon-only"]'
-        # Pego a lista dos botões dropdown
-        list_bot_drop = self.driver.find_elements(By.XPATH, xpath_dropdowns)
-        # Clico no 2º botão dropdown (botão do endereço de origem da devolução) ✓
-        list_bot_drop[1].click()
+        try:
+            # Localizo o RadioButton de Devolucao ✓
+            bot_dev = self.driver.find_element(By.ID, "OriginalAddress_Devolution")
+            # Clico nesse botão ✓
+            bot_dev.click()
+            # E espero um pouco ✓
+            time.sleep(0.2)
+        except:
+            print("Não foi possível clicar na opção devolução")
+        finally:
         #---------------------------------------------------------------------------------------------------------
-
-        
-        # Xpath da DIV que contem o lote do produto a ser devolvido
-        xpath_div_lote = '//div[@style="padding-left: 10px;margin-top: -10px;" and contains(., "Lote: {}")]'.format(array_lote[self.linha])
-        # WebDriver Wait para esperar aparecer a DIV do produto a ser devolvido
-        WebDriverWait(self.driver, 15).until(
-            EC.presence_of_element_located((By.XPATH, xpath_div_lote))
-        )
-        # Apareceu? Então localizo essa DIV
-        div_prod = self.driver.find_element(By.XPATH, xpath_div_lote)
-        # Clico nessa DIV
-        div_prod.click()
-        # e espero um pouco
-        time.sleep(0.2)
-        #---------------------------------------------------------------------------------------------------------
-
-        # Localizo o input de quantidades ✓
-        id_quant = self.driver.find_element(By.ID, "Ammount")
-        # Clico nesse input ✓
-        id_quant.click()
-        #Limpo o input ✓
-        id_quant.clear()
-        # Espero um pouco ✓
-        time.sleep(0.2)
-        # Digito a quantidade certa ✓
-        id_quant.send_keys(array_quant_result[self.linha])
-        #---------------------------------------------------------------------------------------------------------
-
-        #
-        # ANALIZAR NA HR O MELHOR XPATH PARA A POSIÇÃO 
-        # (PROVAVEL QUE SERÁ PARECIDO COM OS ELEMENTOS DE DIVs NO TRECHO DE CÓDIGO ACIMA)
-        #
-        # Clico no 3º botão dropdown (endereço de destino)
-        list_bot_drop[2].click()
-        # Xpath da DIV do endereço de devolução
-        xpath_div_dev = 'xpath_div_dev'
-        # WebDriver Wait para esperar aparecer a (provavel) DIV do endereço de destino
-        WebDriverWait(self.driver, 15).until(
-            EC.presence_of_element_located((By.XPATH, xpath_div_dev))
-        )
-        # Apareceu? Então localizo essa DIV
-        div_dev = self.driver.find_element(By.XPATH, xpath_div_dev)
-        # E clico nessa DIV que aparecer
-        div_dev.click()
-        #---------------------------------------------------------------------------------------------------------
-
-        # Localizo o botao verde ✓
-        botao_verde = self.driver.find_element(By.ID, "MovingProduct_Submit")
-        # Clico no botão verde ✓
-        botao_verde.click()
-        # Adiciono +1 á linha ✓
-        self.contador()
+            try:
+                #
+                # -> Olhar melhor o xpath desses botões (saber se é realmente esse, porque exitiam 2 botões de cada no site offline)
+                #
+                # Xpath dos botões Dropdowns
+                xpath_dropdowns = '//button[@class="pui-button ui-widget ui-state-default ui-corner-right pui-button-icon-only"]'
+                # Pego a lista dos botões dropdown
+                list_bot_drop = self.driver.find_elements(By.XPATH, xpath_dropdowns)
+                # Clico no 2º botão dropdown (botão do endereço de origem da devolução) ✓
+                list_bot_drop[1].click()
+            except:
+                print('Não foi possível clicar no botão do endereço da devolução')
+            finally:
+                #---------------------------------------------------------------------------------------------------------
+                try:
+                    # Xpath da DIV que contem o lote do produto a ser devolvido
+                    xpath_div_lote = '//div[@style="padding-left: 10px;margin-top: -10px;" and contains(., "Lote: {}")]'.format(array_lote[self.linha])
+                    # WebDriver Wait para esperar aparecer a DIV do produto a ser devolvido
+                    WebDriverWait(self.driver, 15).until(
+                        EC.presence_of_element_located((By.XPATH, xpath_div_lote))
+                    )
+                except:
+                    print("Timeout ao esperar pela div com lote: {}".format(array_lote[self.linha]))
+                finally:
+                    # Apareceu? Então localizo essa DIV
+                    try:
+                        div_prod = self.driver.find_element(By.XPATH, xpath_div_lote)
+                        # Clico nessa DIV
+                        div_prod.click()
+                        # e espero um pouco
+                        time.sleep(0.2)
+                    except:
+                        print("Não foi possível clicar na div com o seguinte XPATH: {}".format(xpath_div_lote))
+                    #---------------------------------------------------------------------------------------------------------
+                    finally:
+                        try:
+                            # Localizo o input de quantidades ✓
+                            id_quant = self.driver.find_element(By.ID, "Ammount")
+                            # Clico nesse input ✓
+                            id_quant.click()
+                            #Limpo o input ✓
+                            id_quant.clear()
+                            # Espero um pouco ✓
+                            time.sleep(0.2)
+                            # Digito a quantidade certa ✓
+                            id_quant.send_keys(array_quant_result[self.linha])
+                            self.contador()
+                        except:
+                            print("Não foi possível inserir a quantidade {} no produto {} de lote {}".format(array_quant_result[self.linha],array_cod_prod[self.linha], array_lote[self.linha]))
+                        #---------------------------------------------------------------------------------------------------------
+                        finally:
+                            # Adiciono +1 á linha ✓
+                            print("Contador: ", int(self.linha+2))
--- app4_tela.py
+++ app4_tela.py
@@ -20,14 +20,32 @@ def carregar_navegador():
     nav.carregar_navegador()
 
 def fazer_devolucao():
-    nav.fazer_devolucao(prod.tamanho_devolucao, prod.array_cod_prod, prod.array_lote, prod.array_quant_result)
+    nav.fazer_devolucao(prod.array_cod_prod,prod.array_lote, prod.array_quant_result)
    
 def print_dados_planilha():
     print(nav.dados_planilha(prod))
+    
+def inserir_codigo():
+    nav.inserir_codigo(prod.array_cod_prod)
 
+def sair():
+    window.destroy()
+    
+def pular_linha():
+    nav.contador()
+    label_info.config(text="Linha: "+str(nav.get_contador()+2))
+
+def alterar_linha():
+    nova_linha = int(campo_texto.get())
+    if nova_linha == None or nova_linha == 0:
+        nova_linha = 1
+    nav.alter_contador(nova_linha-2)
+    label_info.config(text="Linha: "+str(int(nav.get_contador()+2)))
 
 
 window = Tk()
+
+
 
 notebook = ttk.Notebook(window) # Um widget que gerencia uma coleção de janelas/displays
 notebook.pack(
@@ -41,12 +59,30 @@ tab2 = Frame(notebook,bg='black')
 notebook.add(tab1,text="Tab 1")
 notebook.add(tab2,text="Tab 2")
 
+# Criando um botão
+botao = Button(tab1, text="Pular linha", command=pular_linha)
+botao.pack(side=TOP, padx=5)
+
+# Criando uma label
+label_info = Label(tab1, text="Linha: "+str(nav.get_contador()))
+label_info.pack(side=TOP, padx=5)
+
+# Criando um campo de texto
+campo_texto = Entry(tab1)
+campo_texto.pack()
+
+# Criando um botão para alterar a label
+botao_alterar = Button(tab1, text="Alterar linha", command=alterar_linha)
+botao_alterar.pack(side=TOP, padx=5)
+
 Label(tab2, text='Inserir dados', width= 22, height=10, bg='light yellow').pack()
 
 Button(tab1, text='Abrir Planilha', font=('Arial', 15), command=abrir_planilha).pack(padx=8, pady=8, fill=BOTH, )
 Button(tab1, text='Carregar Navegador', font=('Arial', 15), command=carregar_navegador).pack(padx=8, pady=8, fill=BOTH)
+Button(tab1, text='Inserir Código', font=('Arial', 15), command=inserir_codigo).pack(padx=8, pady=8, fill=BOTH)
 Button(tab1, text='Fazer Devolução', font=('Arial', 15), command=fazer_devolucao).pack(padx=8, pady=8, fill=BOTH)
 Button(tab1, text='Mostrar quantidades', font=('Arial', 15), command=mostrarQuantidades).pack(padx=8, pady=8, fill=BOTH, )
 Button(tab1, text='Mostrar dados planilha', font=('Arial', 15), command=print_dados_planilha).pack(padx=8, pady=8, fill=BOTH, )
+Button(tab1, text='Sair', font=('Arial', 15), command=sair).pack(padx=8, pady=8, fill=BOTH, )
 
 window.mainloop()
